9ac36499184868c815ae7bef9e34064f3c164f04
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-08-28 13:30:18 -0400

    added and tested link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 
 function App() {
   const [gitHubName, setGitHubName] = useState('')
+  const [gitHubURL, setGitHubURL] = useState('')
 
 
   useEffect(() => {
@@ -11,6 +12,7 @@ function App() {
       .then(res => res.json())
       .then(data => {
         setGitHubName(data.name)
+        setGitHubURL(data.html_url)
       })
   }, [])
 
@@ -18,6 +20,7 @@ function App() {
     <div className="App">
       <h1>Github Profile Info:</h1>
       <h2>{gitHubName}</h2>
+      <a href={gitHubURL}><button>Link to GitHub profile.</button></a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,28 @@
 
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App'
+require('jest-fetch-mock').enableMocks()
+
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+})
 
 
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ name: 'coder' }))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+})
+
+test("test button for text content and href that is obtained from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ html_url: 'http://github.com/learningToCode1234' }))
+    render(<App />)
+    const gitHubURL = await waitFor(() => screen.getByRole('link'))
+    expect(gitHubURL).toHaveAttribute('href', 'http://github.com/learningToCode1234')
+    const button = await waitFor(() => screen.getByRole('button'))
+    expect(button).toHaveTextContent("Link to GitHub profile.")
+})
