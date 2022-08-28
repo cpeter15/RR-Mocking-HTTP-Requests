ba29001f4208b2c466407adef2b9f06190b86772
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-08-28 12:50:22 -0400

    added some tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,17 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+require('jest-fetch-mock').enableMocks()
+
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+})
+
+
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ name: 'coder' }))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+})
